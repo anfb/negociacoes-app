121b876ba5241470b45441aac0f2b647cbfc23aa
anfb <adriely.nara@gmail.com>
2021-07-20 06:41:04 -0300

    Protecting the code from malicious scripts.

--- app/views/View.ts
+++ app/views/View.ts
@@ -1,15 +1,24 @@
 export abstract class View<T>
 {
     protected elementview :HTMLElement;
+    private escape = false;
 
-    constructor(elementSelector :string)
+    constructor(elementSelector :string, escape?: boolean)
     {
         this.elementview = document.querySelector(elementSelector);
+
+        if (escape) {
+            this.escape = escape;
+        }
     }
 
     public updateView(elementModel: T) :void
     {
-        const template = this.template(elementModel);
+        let template = this.template(elementModel);
+
+        if (this.escape) {
+            template = template.replace(/<script>[\s\S]*?<\/script>/, '');
+        }
    
         //innerHTML used to update the div negotiationView
         this.elementview.innerHTML = template;
